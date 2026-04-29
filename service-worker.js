c2287cd1163ddf692525dd788df088adffdd8be6
isildurschmidt-spec <isildurschmidt@gmail.com>
2026-04-29 14:42:11 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "binaural-pwa-v1";
+const CACHE = "binaural-pwa-v2";
 const ASSETS = ["./", "index.html", "manifest.json", "icon.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
